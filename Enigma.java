d69e305b07c67951d52280003d572986ac8d8a0f
wtokarz80 <w.tokarz80gmail.com>
2020-03-09 11:41:27 +0100

    declare main variables

--- Enigma.java
+++ Enigma.java
@@ -1,3 +1,48 @@
-class Enigma {
-    
+public class Enigma {
+
+    public static void main(String[] args){
+        String userMode = args[0];
+        String userCipher = args[1];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
 }
