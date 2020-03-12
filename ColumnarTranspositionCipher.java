32b6d03eddb5146c9cc84da08ebcb9a5ec53e47a
Alek <sforaaleksander@gmail.com>
2020-03-12 15:21:48 +0100

    decipher columnarytransposition

--- ColumnarTranspositionCipher.java
+++ ColumnarTranspositionCipher.java
@@ -1,34 +1,37 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 class ColumnarTranspositionCipher {
     private static String enciphered = "";
     private static String deciphered = "";
-    private static char[] ALPHABET = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                                        'N', 'O', 'P', 'Q', 'R','S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+    private static char[] ALPHABET = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+            'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
     private static int rowsNum;
     private static char[] theMessage;
     private static char[] theKey;
+    private static char[] sortedKey;
     private static char[][] table;
+    private static char[][] decipherTable;
     private static char[][] sortedColumns;
     private static char[][] transpositionedColumns;
+    private static int[] order;
 
     public static void columnarTransposition(String userMode, String userKey) {
         initialize(userKey);
+        if (userMode.equals("-E")) {
+            createTable();
+            encipher();
+        } else if (userMode.equals("-D")) {
+            decipher();
+        }
 
-        transpositionedColumns = transpositionColumns();
-        sortedColumns = sortColumns(transpositionedColumns);
-        printTables();
-        enciphered = collectLetters();
-        System.out.println(" ");
-        System.out.println("FINAL RESULT");
-        System.out.println(enciphered);
     }
 
     private static void initialize(String userKey) {
         theKey = userKey.toCharArray();
         gatherInput();
         rowsNum = (int) Math.ceil(theMessage.length / theKey.length) + 1;
-        createTable();
     }
 
     private static void gatherInput() {
@@ -58,38 +61,33 @@ class ColumnarTranspositionCipher {
         }
     }
 
-    private static void printTables() {
+    private static void printRegularTable(char[][] tableToPrint) {
         System.out.println(" ");
-        System.out.println("REGULAR TABLE");
         for (int i = 0; i < rowsNum; i++) {
             for (int j = 0; j < theKey.length; j++) {
-                System.out.print(table[i][j]);
+                System.out.print(tableToPrint[i][j]);
             }
             System.out.println("");
         }
+
+    }
+
+    private static void printModifiedTable(char[][] tableToPrint) {
         System.out.println(" ");
         System.out.println("TRANSPOSITIONED COLUMNS");
         for (int i = 0; i < theKey.length; i++) {
             for (int j = 0; j < rowsNum; j++) {
-                System.out.print(transpositionedColumns[i][j]);
-            }
-            System.out.println("");
-        }
-        System.out.println(" ");
-        System.out.println("SORTED COLUMNS");
-        for (int i = 0; i < theKey.length; i++) {
-            for (int j = 0; j < rowsNum; j++) {
-                System.out.print(sortedColumns[i][j]);
+                System.out.print(tableToPrint[i][j]);
             }
             System.out.println("");
         }
     }
 
-    private static char[][] transpositionColumns() {
-        char[][] transpositionedColumns = new char[theKey.length][rowsNum];
+    private static char[][] transpositionColumns(char[][] tableToTranspose) {
+        transpositionedColumns = new char[theKey.length][rowsNum];
         for (int j = 0; j < theKey.length; j++) {
             for (int i = 0; i < rowsNum; i++) {
-                transpositionedColumns[j][i] += table[i][j];
+                transpositionedColumns[j][i] += tableToTranspose[i][j];
             }
         }
         return transpositionedColumns;
@@ -117,5 +115,88 @@ class ColumnarTranspositionCipher {
             }
         }
         return finalCipher;
+    }
+
+    private static void encipher() {
+        transpositionedColumns = transpositionColumns(table);
+        sortedColumns = sortColumns(transpositionedColumns);
+        printRegularTable(table);
+        printModifiedTable(table);
+        enciphered = collectLetters();
+        System.out.println(enciphered);
+    }
+
+    private static void decipher() {
+        createSortedKey();
+        System.out.println(sortedKey);
+        System.out.println(findOrder());
+        createTableDecipher();
+        transpositionedColumns = transpositionColumns(decipherTable);
+        sortedColumns = sortColumnsDecipher(transpositionedColumns);
+        printRegularTable(decipherTable);
+        printModifiedTable(transpositionedColumns);
+        printModifiedTable(sortedColumns);
+        deciphered = readDecipher();
+        System.out.println(deciphered);
+
+    }
+
+    private static void createSortedKey() {
+        String keyString = "";
+        for (char alpha : ALPHABET) {
+            for (char letter : theKey) {
+                if (alpha == letter) {
+                    keyString += letter;
+                }
+            }
+        }
+        sortedKey = keyString.toCharArray();
+    }
+
+    private static void createTableDecipher() {
+        decipherTable = new char[rowsNum][theKey.length];
+        for (int i = 0; i < sortedKey.length; i++) {
+            decipherTable[0][i] = sortedKey[i];
+        }
+        int index = 0;
+        for (int i = 0; i < theKey.length; i++) {
+            for (int j = 1; j < rowsNum; j++) {
+                decipherTable[j][i] = theMessage[index];
+                index += 1;
+            }
+        }
+    }
+
+    private static int[] findOrder() {
+        List<Integer> orderList = new ArrayList<Integer>();
+        for (char letter : theKey) {
+            for (int i = 0; i < theKey.length; i++) {
+                if (sortedKey[i] == letter) {
+                    orderList.add(i);
+                }
+            }
+        }
+        order = new int[orderList.size()];
+        for (int i = 0; i < order.length; i++) {
+            order[i] = orderList.get(i).intValue();
+        }
+        return order;
+    }
+
+    private static char[][] sortColumnsDecipher(char[][] decipherTable) {
+        sortedColumns = new char[theKey.length][rowsNum];
+        for (int i = 0; i < order.length; i++) {
+            sortedColumns[i] = decipherTable[order[i]];
+        }
+        return sortedColumns;
+    }
+
+    private static String readDecipher() {
+        for (int i = 1; i < rowsNum; i++) {
+            for (int j = 0; j < theKey.length; j++) {
+                deciphered += sortedColumns[j][i];
+            }
+        }
+        return deciphered;
     }
 }
